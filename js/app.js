ed5c80f1307cde19bb32f3102b0bcf6a8e592517
Kevin Shields <kshields551@gmail.com>
2018-11-15 19:34:00 -0500

    This is the beginning of unit 3 with the first input automatically highlighting already

---
+++ js/app.js
@@ -0,0 +1,3 @@
+$("#name").focus(); //this focuses the name on the main textbox
+
+//Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
