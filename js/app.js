02f62faaaa9941c8232ebb9d87e99f7415ebfbc3
kshields51 <kshields551@gmail.com>
2018-11-19 17:27:01 -0500

    this has the completed calculator will start on the cc now

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+function textComparison (arg1, arg2) {
+    if (arg1 === arg2) {
+        return true
+    } else {
+        return false
+    }
+}
+
 $("#name").focus(); //this focuses the name on the main textbox
 /******************
  Job Role
@@ -61,24 +69,91 @@ $('#design').on('change', (function(event) {
  Greying out workshops that are at the same day and time of selected. Upon uncheck competing activities are no
  longer disabled
  ****************** */
+const activities = $('.activities').children()
+const finalCost =$('.activities').after('<h1 class="total-cost"></h1>')
+$('fieldset').children().on('change', function(event){
 
-$('fieldset').on('change', function(event){
-    const checkboxText = $(event.target).parent().text()
-    const isChecked = event.target.checked
-    console.log(checkboxText);
-    $('.activities').children().each(function() {
-        if($(this).text() === checkboxText && isChecked) {
-            console.log('hello')
-            event.preventDefault();
-            //alert('Activity with the same timeslot already selected. Please choose a different activity.')
+    // let cost = $('input:checkbox:checked').length * 100
+    const targetIndex = ($(event.target).parent().index())
+    function totalCost () {
+        let cost = $('input:checkbox:checked').length * 100
+        if ($(activities).eq(1).index() == 1 && $(activities).eq(1).children().prop('checked')){
+            cost +=100
+            return cost 
+        } else {
+            
+            return cost
         }
-    })
+    }
+    //im trying to create and append after the paragraph a total cost line that is dynamcically updated i have it but its not being updated
+    console.log(totalCost()+ 'hi')
+    const $postedCost = $('<p class="total-cost">Total Cost: $'+(totalCost()).toString()+'</p>')
 
-})
+    $('.total-cost').replaceWith($postedCost);
+    console.log(totalCost()+ "after posting")
+    switch (targetIndex) {
+        case 1:
+            break;
+        case 2:
+            if ($(event.target).is(':checked')) {
+                
+                activities.eq(4).html('<input type="checkbox" name="express"><strike> Express Workshop — Tuesday 9am-12pm, $100</strike>')
+                activities.eq(4).children().attr("disabled", "disabled");
+            
+            } else {
+                activities.eq(4).html('<input type="checkbox" name="express"> Express Workshop — Tuesday 9am-12pm, $100')
+                activities.eq(4).removeAttr("disabled");
+            }
+            break;
+        case 3:
+        if ($(event.target).is(':checked')) {
+                
+            activities.eq(5).html('<input type="checkbox" name="node"><strike> Node.js Workshop — Tuesday 1pm-4pm, $100</strike>')
+            activities.eq(5).children().attr("disabled", "disabled");
+        
+        } else {
+            activities.eq(5).html('<input type="checkbox" name="node"> Node.js Workshop — Tuesday 1pm-4pm, $100')
+            activities.eq(5).removeAttr("disabled");
+        }
+            break;
+        case 4:
+        if ($(event.target).is(':checked')) {
+                
+            activities.eq(2).html('<input type="checkbox" name="js-frameworks"><strike> JavaScript Frameworks Workshop — Tuesday 9am-12pm, $100</strike>')
+            activities.eq(2).children().attr("disabled", "disabled");
+        
+        } else {
+            activities.eq(2).html('<input type="checkbox" name="js-frameworks"> JavaScript Frameworks Workshop — Tuesday 9am-12pm, $100')
+            activities.eq(2).removeAttr("disabled");   
+        }
+            break;
+        case 5:
+        if ($(event.target).is(':checked')) {
+                
+            activities.eq(3).html('<input type="checkbox" name="js-libs"><strike> JavaScript Libraries Workshop — Tuesday 1pm-4pm, $100</strike>')
+            activities.eq(3).children().attr("disabled", "disabled");
+        
+        } else {
+            activities.eq(3).html('<input type="checkbox" name="js-libs"> JavaScript Libraries Workshop — Tuesday 1pm-4pm, $100')
+            activities.eq(3).removeAttr("disabled");
+        }
+            break;
+        default:
+            // null
+}}
+
+);
 
  /******************
  Running total cost of all selected activities
  ****************** */
+console.log('hello')
+//console.log($('input:checkbox:checked').length);
+
+
+
+
+
 
  /******************
  Display payment selections basesd on the payment option chosen in the select menu. Credit card is default.
